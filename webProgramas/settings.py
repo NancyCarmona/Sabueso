90acaee1c7b8f3e18d23a77598446027aaaef44a
NancyCarmona <nancyacc13@hotmail.com>
2020-12-21 19:13:48 -0600

    ...

--- webProgramas/settings.py
+++ webProgramas/settings.py
@@ -140,6 +140,6 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
 
 
-#STATICFILES_DIRS = (
-    #os.path.join(BASE_DIR, 'static'), #aqui se buscan los archivos estaticos
-#) 
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'), #aqui se buscan los archivos estaticos
+) 
