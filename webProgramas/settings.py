1b0b7149ff5918584be282cd8111072ffb292699
NancyCarmona <nancyacc13@hotmail.com>
2020-12-21 20:08:02 -0600

    ..

--- webProgramas/settings.py
+++ webProgramas/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 #agregado para pythonanywhere
-STATIC_ROOT = os.path.join(BASE_DIR, 'static') 
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/site') 
 
 
 STATICFILES_DIRS = (
